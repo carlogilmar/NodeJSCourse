40b32dd8740d263acc5949ab3a64c43ab1024fbf
Carlo Gilmar <carlogilmar12@gmail.com>
2022-04-15 22:14:09 -0500

    Adding spiderverse folder

---
+++ examples/week3/lives/spiderverse/app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman{
+  constructor(name, age, actor, movies, studio){
+    this.name = name
+    this.age = age
+    this.actor = actor
+    this.movies = movies
+    this.studio = studio
+  }
+}
+
+module.exports = Spiderman
---
+++ examples/week3/lives/spiderverse/test/spiderman.test.js
@@ -0,0 +1,17 @@
+const Spiderman = require('./../app/spiderman')
+
+describe("Unit Tests for Spiderman class", () => {
+	test('1) Create an spiderman object', () => {
+
+		// Aquí escribimos el código que queremos usar tal cuál
+		// Quiero poder instanciar un objeto Spiderman con esta información
+		const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+		// Validamos que este código funcione de la forma esperada
+		expect(andrewGarfield.name).toBe("Spiderman Sony")
+		expect(andrewGarfield.age).toBe(31)
+		expect(andrewGarfield.actor).toBe("Andrew Garfield")
+		expect(andrewGarfield.movies).toBe(2)
+		expect(andrewGarfield.studio).toBe("Sony")
+	});
+})
