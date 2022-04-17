b31466b8dc0a9fe072baf1d09f095b942967f3a0
Carlo Gilmar <carlogilmar12@gmail.com>
2022-04-17 01:40:42 -0500

    Adding express api example

---
+++ examples/week3/content/express_api/app_api.js
@@ -0,0 +1,52 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+  console.log(`Api Explorers GET ALL requests ${new Date()}`)
+  console.log(`Getting explorer with id ${req.params.id}`)
+  const explorer1 = {id: 1, name: "Carlo"}
+  const explorer2 = {id: 1, name: "Carlo"}
+  const explorer3 = {id: 1, name: "Carlo"}
+  const explorer4 = {id: 1, name: "Carlo"}
+  const explorers = [explorer1, explorer2, explorer3, explorer4]
+  res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers GET request ${new Date()}`)
+  console.log(`Getting explorer with id ${req.params.id}`)
+  const explorer = {id: 1, name: "Carlo"}
+  res.status(200).json(explorer)
+})
+
+app.post('/v1/explorers', (req, res) => {
+  console.log(`Api Explorers POST request ${new Date()}`)
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(201).json({message: "Created"})
+})
+
+app.put('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers PUT request ${new Date()}`)
+  console.log(`Update explorer with id ${req.params.id}`)
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(200).json({message: "Updated!"})
+})
+
+app.delete('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers DELETE request ${new Date()}`)
+  console.log(`Delete explorer with id ${req.params.id}`)
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(200).json({message: "Deleted"})
+})
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
+
