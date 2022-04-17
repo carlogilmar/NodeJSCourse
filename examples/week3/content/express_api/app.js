9a12fb1e12d8b5088144abc77fdf2b0503d7e397
Carlo Gilmar <carlogilmar12@gmail.com>
2022-04-16 23:36:57 -0500

    Agregando express api

---
+++ examples/week3/content/express_api/app.js
@@ -0,0 +1,17 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// path inicial, responderá a la url localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
