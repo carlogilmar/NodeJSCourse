b5d77c9d0555de17b18d6231fb47a8379b1b3763
Carlo Gilmar <carlogilmar12@gmail.com>
2022-04-17 01:42:49 -0500

    Renamig

--- examples/week3/content/express_api/app.js
+++ examples/week3/content/express_api/app.js
@@ -2,34 +2,48 @@
 const express = require('express')
 // App de Express
 const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
-// path inicial, responderá a la url localhost:3000
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+  console.log(`Api Explorers GET ALL requests ${new Date()}`)
+  console.log(`Getting explorer with id ${req.params.id}`)
+  const explorer1 = {id: 1, name: "Carlo"}
+  const explorer2 = {id: 1, name: "Carlo"}
+  const explorer3 = {id: 1, name: "Carlo"}
+  const explorer4 = {id: 1, name: "Carlo"}
+  const explorers = [explorer1, explorer2, explorer3, explorer4]
+  res.status(200).json(explorers)
 })
 
-// Respondiendo texto
-// localhost:3000/launchx
-app.get('/launchx', (req, res) => {
-    res.send('Bievenidos a launchx')
+app.get('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers GET request ${new Date()}`)
+  console.log(`Getting explorer with id ${req.params.id}`)
+  const explorer = {id: 1, name: "Carlo"}
+  res.status(200).json(explorer)
 })
 
-// Regresando un objeto
-// localhost:3000/explorersInNode
-app.get('/explorersInNode', (req, res) => {
-  const explorer = {name: "Explorer", msg: "Hello"}
-  res.send(explorer)
+app.post('/v1/explorers', (req, res) => {
+  console.log(`Api Explorers POST request ${new Date()}`)
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(201).json({message: "Created"})
 })
 
-// Query Params: Recibir parámetros por la url
-// http://localhost:3000/explorers/carlo
-// req.params = {"explorerName":"carlo"}
-app.get('/explorers/:explorerName', (req, res) => {
-    res.send(req.params)
+app.put('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers PUT request ${new Date()}`)
+  console.log(`Update explorer with id ${req.params.id}`)
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(200).json({message: "Updated!"})
 })
 
+app.delete('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers DELETE request ${new Date()}`)
+  console.log(`Delete explorer with id ${req.params.id}`)
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(200).json({message: "Deleted"})
+})
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
---
+++ examples/week3/content/express_basic/app.js
@@ -0,0 +1,38 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// path inicial, responderá a la url localhost:3000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Respondiendo texto
+// localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+    res.send('Bievenidos a launchx')
+})
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+  const explorer = {name: "Explorer", msg: "Hello"}
+  res.send(explorer)
+})
+
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/carlo
+// req.params = {"explorerName":"carlo"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
+
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
+
